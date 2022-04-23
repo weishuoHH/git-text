73d3a7c0198ba9a1f573d7f81200cc4199ba94df
weishuo <weishuo@weishuo.com>
2022-04-23 11:28:45 +0800

    second commit

--- src/main/java/com/weishuo/git/GitTes.java
+++ src/main/java/com/weishuo/git/GitTes.java
@@ -3,5 +3,6 @@ package com.weishuo.git;
 public class GitTes {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
