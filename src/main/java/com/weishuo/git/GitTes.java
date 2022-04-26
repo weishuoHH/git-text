19a2e14d1e33c61ecd3905d662bfb1b5e022664f
weishuo <weishuo@weishuo.com>
2022-04-26 19:54:38 +0800

    push3 test

--- src/main/java/com/weishuo/git/GitTes.java
+++ src/main/java/com/weishuo/git/GitTes.java
@@ -10,5 +10,6 @@ public class GitTes {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("push2 test");
+        System.out.println("push3 test");
     }
 }
